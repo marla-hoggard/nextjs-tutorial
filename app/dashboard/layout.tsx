282069d92c5d9150466cbe0e980fc671dcb3448d
Marla Hoggard <marla@switchboard.app>
2024-08-24 18:21:33 -0400

    Add, create, delete invoices

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+export const experimental_ppr = true;
+
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   return (
     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
